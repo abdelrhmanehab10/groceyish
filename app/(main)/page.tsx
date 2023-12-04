19043f1ca413275f09a7141f5753657efd740791
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2023-12-04 14:20:44 +0200

    fix: remove unnecessary imports from main/page.tsx

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,9 @@
 import { db } from "@/lib/db";
 import AdsSpace from "./_components/ads-space";
 import BestSellSection from "./_components/best-sell/best-sell-section";
-import CategoryProductCard from "./_components/category/category-card";
 import Hero from "./_components/hero";
 import ListItems from "./_components/list-items/list-items";
 import Offer from "./_components/offer";
-import ProductCard from "./_components/product/product";
 import CategorySection from "./_components/category/category-section";
 
 export default async function Home() {
