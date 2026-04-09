b5c060574a258cb7cfc2b1e3a61c2540d515babc
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2026-04-09 13:57:21 +0200

    Format middleware.ts (auto-formatted)

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import type { NextRequest, NextFetchEvent } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default function middleware(request: NextRequest) {
+export default function middleware(
+  request: NextRequest,
+  event: NextFetchEvent,
+) {
   // Workaround for Next.js middleware bypass vulnerability
   // Prevent external requests with x-middleware-subrequest header
   if (request.headers.get("x-middleware-subrequest")) {
@@ -14,7 +17,7 @@ export default function middleware(request: NextRequest) {
 
   return authMiddleware({
     publicRoutes: ["/", "/api/uploadthing", "/api/user"],
-  })(request);
+  })(request, event);
 }
 
 export const config = {
